35f3d0d98b1d29b5c9c13a6c286aa0869050d2b3
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-02-08 16:48:04 +0100

    add greeting & Favicon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
-function init(){
-    
+function init() {
+    includeHTML();
+    welcomeGreeting();
 }
 
 function navToSignIn() {
@@ -28,4 +29,31 @@ async function includeHTML() {
         }
     }
 }
+
+
+function greetingTime() {
+    let actualTime = new Date();
+    let time = actualTime.getHours();
+
+    if (time >= 5 && time < 12) {
+        return "Guten Morgen";
+    } else if (time >= 12 && time < 17) {
+        return "Guten Mittag";
+    } else if (time >= 17 && time < 24) {
+        return "Guten Abend";
+    } else {
+        return "Gute Nacht";
+    }
+    
+}
+
+function welcomeGreeting() {
+    let greet = document.getElementById('greet-time');
+    greet.innerHTML = '';
+    greet.innerHTML += greetingTime();
+   
+}
+
+
+
 

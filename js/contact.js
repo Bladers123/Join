3cdec00729307dde513ef3ba0149188a8870a684
Julian Fichtl <64958181+JulianFichtl@users.noreply.github.com>
2024-02-09 13:43:46 +0100

    contacts page

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function openContact() {
+    document.getElementById("contactOverlay").classList.remove("d-none");
+}
+
+function closeContact() {
+    document.getElementById("contactOverlay").classList.add("d-none");
+}

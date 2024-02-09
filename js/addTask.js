749f3a308c437e6f03c48db84fdc7d44bb7d0bd2
Bladers123 <tristangehring123@hotmail.de>
2024-02-09 23:14:55 +0100

    combox in add_task

--- js/addTask.js
+++ js/addTask.js
@@ -1,18 +1,46 @@
-function initTask(){
-    loadComboBox();
+let items = [
+    { name: "Mike", imgUrl: "../img/person.svg" },
+    { name: "Sabine", imgUrl: "../img/person.svg" },
+    { name: "Felix", imgUrl: "../img/person.svg" },
+    { name: "Lars", imgUrl: "../img/person.svg" },
+    { name: "Charly", imgUrl: "../img/person.svg" },
+    { name: "Ari", imgUrl: "../img/person.svg" },
+    { name: "James", imgUrl: "../img/person.svg" },
+    { name: "Sibi", imgUrl: "../img/person.svg" },
+    { name: "Mia", imgUrl: "../img/person.svg" },
+];
+
+
+function initTask() {
+    rotateIcon();
 }
 
-function loadComboBox(){
-    let checkList = document.getElementById('list1');
-    if(checkList) {
-        let anchor = checkList.getElementsByClassName('anchor')[0];
-        if(anchor) {
-            anchor.onclick = function(evt) {
-              if (checkList.classList.contains('visible'))
-                checkList.classList.remove('visible');
-              else
-                checkList.classList.add('visible');
-            }
-        }
-    }
+function openCheckBoxArea() {
+    let checkBoxItems = document.getElementById('checkBoxItems');
+    rotateIcon();
+    if (checkBoxItems.innerHTML.trim() !== '')
+        checkBoxItems.innerHTML = '';
+    else
+        checkBoxItems.innerHTML = getCeckBoxAreaTemplate();
 }
+
+function getCeckBoxAreaTemplate() {
+    return items.map(item => {
+        return /*html*/`
+            <div class="item">
+                <img src="${item.imgUrl}" alt="Picture of ${item.name}">
+                <label>${item.name}</label>
+                <input class="checkbox" type="checkbox">
+            </div>
+        `;
+    }).join('');
+}
+
+function rotateIcon() {
+    let icon = document.getElementById('navigation');
+    if (icon.style.transform === 'rotate(180deg)')
+        icon.style.transform = '';
+    else
+        icon.style.transform = 'rotate(180deg)';
+}
+

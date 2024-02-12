7729a7a18d84b8f289755bd8d56f41eda32c5c1b
Julian Fichtl <64958181+JulianFichtl@users.noreply.github.com>
2024-02-12 18:44:17 +0100

    Render contacts is working

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,53 @@
 let oldContacts = [
     {
-        "name": "Tristan Gehrig",
-        "email": "tristan@gmail.com",
-        "tel": "017612312333",
+        name: "Tristan Gehrig",
+        email: "tristan@gmail.com",
+        tel: "017612312333",
     },
     {
-        "name": "Julian Fichtl",
-        "email": "julian@gmail.com",
-        "tel": "01529483027",
+        name: "Julian Fichtl",
+        email: "julian@gmail.com",
+        tel: "01529483027",
     },
     {
-        "name": "Rabia Ürkmez",
-        "email": "rabia@gmail.com",
-        "tel": "017612312333",
+        name: "Rabia Ürkmez",
+        email: "rabia@gmail.com",
+        tel: "017612312333",
     },
     {
-        "name": "Agathe Bauer",
-        "email": "igotthe@bauer.com",
-        "tel": "071319991122",
+        name: "Agathe Bauer",
+        email: "igotthe@bauer.com",
+        tel: "071319991122",
     },
     {
-        "name": "Bertold Cislewitz",
-        "email": "familie@galgant.de",
-        "tel": "017612312333",
+        name: "Bertold Cislewitz",
+        email: "familie@galgant.de",
+        tel: "017612312333",
     },
     {
-        "name": "Christine Dorst",
-        "email": "hab@durst.com",
-        "tel": "017612312333",
+        name: "Christine Dorst",
+        email: "hab@durst.com",
+        tel: "017612312333",
     },
     {
-        "name": "Dominik Emmerich",
-        "email": "schimmelich@t-online.de",
-        "tel": "015112314027",
+        name: "Dominik Emmerich",
+        email: "schimmelich@t-online.de",
+        tel: "015112314027",
     },
     {
-        "name": "Erol Fleischer",
-        "email": "fleischi@gmail.com",
-        "tel": "017112954562",
+        name: "Erol Fleischer",
+        email: "fleischi@gmail.com",
+        tel: "017112954562",
     },
     {
-        "name": "Frank Gül",
-        "email": "francis-gulle@yahoo.de",
-        "tel": "017612312333",
+        name: "Frank Gül",
+        email: "francis-gulle@yahoo.de",
+        tel: "017612312333",
     },
     {
-        "name": "Gustav Holm",
-        "email": "hoelmchen@gmx.de",
-        "tel": "01744975233",
+        name: "Gustav Holm",
+        email: "hoelmchen@gmx.de",
+        tel: "01744975233",
     },
 ];
 let letters = [];
@@ -61,20 +61,22 @@ function closeContact() {
 }
 
 function renderOldContacts() {
-    let renderContact = document.getElementById('contactName');
+    let renderContact = document.getElementById("contactName");
     let currentLetter = null;
-    renderContact.innerHTML = '';
+    renderContact.innerHTML = "";
 
     oldContacts.sort((a, b) => a.name.localeCompare(b.name)); // sortiert das Array erst einmal alphabetisch
-    
 
     for (let i = 0; i < oldContacts.length; i++) {
         const oldContact = oldContacts[i];
-        let name = oldContact['name'];
-        let mail = oldContact['email'];
-        let initials = name.split(" ").map((n) => n[0]).join("");
+        let name = oldContact["name"];
+        let mail = oldContact["email"];
+        let initials = name
+            .split(" ")
+            .map((n) => n[0])
+            .join("");
         let sortedByLetter = name.charAt(0);
-        
+
         if (sortedByLetter !== currentLetter) {
             renderContact.innerHTML += `
             <div class="letterWrapper">
@@ -82,25 +84,8 @@ function renderOldContacts() {
                 <div class="divider"></div>
             </div>
             `;
-        } else {
-            console.log('versuchs nochmal')
-            
         }
-        sortContacts(renderContact, sortedByLetter, name, mail, initials, i);
-        showContact(i);
-        
-    }
-
-}
-
-function sortContacts(renderContact, sortedByLetter, name, mail, initials, i) {
-
-    for (let l = 0; l < letters.length; l++) {
-        const oneLetter = letters[l];
-        
-
-        if (oneLetter !== sortedByLetter) {
-            renderContact.innerHTML += `    
+        renderContact.innerHTML += `    
                 <div onclick="showContact(${i})" class="name">
                     <div class="initialCircle">${initials}</div>
                         <div class="contactWrapper">
@@ -108,39 +93,37 @@ function sortContacts(renderContact, sortedByLetter, name, mail, initials, i) {
                         <div class="email">${mail}</div>
                     </div>
                 </div>`;
-        } else {
-            console.log('Heul leise')
-        }
     }
-
 }
 
 function showContact(i) {
     let selectedName = oldContacts[i];
-    let name = selectedName['name'];
-    let mail = selectedName['email'];
-    let number = selectedName['tel'];
-    let initials = name.split(" ").map((n) => n[0]).join("");
+    let name = selectedName["name"];
+    let mail = selectedName["email"];
+    let number = selectedName["tel"];
+    let initials = name
+        .split(" ")
+        .map((n) => n[0])
+        .join("");
     let letter = name.charAt(0);
 
     letters.push(letter);
-   // console.log(letter)
-    
-    let contact = document.getElementById('open-contact');
-    contact.innerHTML = '';
-    contact.innerHTML += generateHTML(name, mail, number, initials);
+    // console.log(letter)
 
+    let contact = document.getElementById("open-contact");
+    contact.innerHTML = "";
+    contact.innerHTML += generateHTML(name, mail, number, initials);
 }
 
 async function createContact() {
-    let name = document.getElementById('contact-name');
-    let mail = document.getElementById('contact-email');
-    let tel = document.getElementById('contact-tel');
+    let name = document.getElementById("contact-name");
+    let mail = document.getElementById("contact-email");
+    let tel = document.getElementById("contact-tel");
 
     let newContact = {
-        "name": name.value,
-        "mail": mail.value,
-        "tel": tel.value
+        name: name.value,
+        mail: mail.value,
+        tel: tel.value,
     };
 
     oldContacts.push(newContact);
@@ -149,12 +132,10 @@ async function createContact() {
 
     renderOldContacts();
 
-    name.value = '';
-    mail.value = '';
-    tel.value = '';
-
+    name.value = "";
+    mail.value = "";
+    tel.value = "";
 }
-
 
 function deleteContact(i) {
     oldContacts.splice(i, 1);
@@ -163,20 +144,17 @@ function deleteContact(i) {
     renderOldContacts();
 }
 
-
-
 /*async function loadContacts() {
     oldContacts = JSON.parse(await getItem('oldContacts'));
 
 }*/
 
 function openPopUp() {
-    document.getElementById('pop-up').classList.remove('d-none');
-    document.getElementById('pop-up').classList.add('d-flex');
+    document.getElementById("pop-up").classList.remove("d-none");
+    document.getElementById("pop-up").classList.add("d-flex");
 }
 
-
 function closePopUp() {
-    document.getElementById('pop-up').classList.add('d-none');
-    document.getElementById('pop-up').classList.remove('d-flex');
+    document.getElementById("pop-up").classList.add("d-none");
+    document.getElementById("pop-up").classList.remove("d-flex");
 }

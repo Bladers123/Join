64bb0f23f95596fb28371a9448ed1244275929bb
Julian Fichtl <64958181+JulianFichtl@users.noreply.github.com>
2024-02-14 20:13:57 +0100

    toUpperCase

--- js/contact.js
+++ js/contact.js
@@ -90,7 +90,7 @@ function renderOldContacts() {
             .split(" ")
             .map((n) => n[0])
             .join("");
-        let sortedByLetter = name.charAt(0);
+        let sortedByLetter = name.charAt(0).toUpperCase();
 
         if (sortedByLetter !== currentLetter) {
             currentLetter = sortedByLetter;
@@ -142,7 +142,7 @@ async function createContact() {
 
     let newContact = {
         name: name.value,
-        mail: mail.value,
+        email: mail.value,
         tel: tel.value,
     };
 

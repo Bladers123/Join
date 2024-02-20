f0cccfe81c8aed4b5dc51c6044965afc05988876
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-02-20 08:47:28 +0100

    not finish

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 async function init() {
    await includeHTML();
+   showAmounts();
 }
 
 async function includeHTML() {
--- js/summary.js
+++ js/summary.js
@@ -2,11 +2,11 @@ function greetingTime() {
     let actualTime = new Date();
     let time = actualTime.getHours();
 
-    if (time >= 5 && time < 12) {
+    if (time >= 5 && time < 11) {
         return "Guten Morgen";
-    } else if (time >= 12 && time < 17) {
+    } else if (time >= 11 && time < 17) {
         return "Guten Mittag";
-    } else if (time >= 17 && time < 24) {
+    } else if (time >= 17 && time < 23) {
         return "Guten Abend";
     } else {
         return "Gute Nacht";
@@ -19,4 +19,25 @@ function welcomeGreeting() {
     greet.innerHTML = '';
     greet.innerHTML += greetingTime();
    
+}
+
+function concat() {
+    let getAllArrays = tasks.concat(currentTask);
+}
+
+function showAmounts() {
+    for (let t = 0; t < tasks.length; t++) {
+        const task = tasks[t];
+        progress = task['progress']
+
+        console.log(progress)
+    }
+
+
+    // let toDoAmount = document.getElementById('to-do-amount');
+    // toDoAmount.innerHTML = '';
+    // toDoAmount.innerHTML += tasks.length
+
+    // console.log(toDoAmount)
+    
 }

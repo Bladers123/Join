283164e56819d53dd53b40d1af607cec10a9c749
Julian Fichtl <64958181+JulianFichtl@users.noreply.github.com>
2024-02-25 10:27:11 +0100

    Little changes

--- js/side_and_nav_bar.js
+++ js/side_and_nav_bar.js
@@ -1,3 +1,9 @@
-function initSideAndNavBar(){
-   
+function addActiveState(path, id) {
+    const currentPageUrl = window.location.pathname;
+
+    if (path === currentPageUrl) {
+        document.getElementById(id).classList.add("active");
+    }
+
+    console.log(currentPageUrl);
 }
